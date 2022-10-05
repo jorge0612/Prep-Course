f580ac48a1381cbf8b439b7941d7cbb99c15c623
jorge parra <jorge_j_0612@hotmail.com>
2022-10-05 11:36:11 -0500

    faltan 8 ejercicios

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -107,6 +107,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+ var maximo = numeros[0];
+  for (var i = 1; i < numeros.length; i++ ){
+    if (numeros [i] > maximo){
+      maximo = numeros [i];
+    }
+  }
+  return maximo;
 }
 
 
@@ -114,13 +121,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if ( arguments.length < 1) return 0;
+  var total = 1;
+  for ( var i = 0; i < arguments.length; i ++){
+    total = total * arguments[i];
+  }
+  return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var retorno = 0;
+for (i = 0; i < arreglo.length; i++){
+    if (arreglo[i] > 18){
+      retorno++;
+}
+}
+ return retorno;
 }
 
 
@@ -129,8 +148,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
-} 
+  if ( numeroDeDia=== 1 || numeroDeDia === 7 ){
+    return "Es fin de semana";
+  } 
+     return "Es dia Laboral";
+  }
+
 
 
 function empiezaConNueve(n) {
